cb50abff5c4ead7dd18c9d1ff54e2691cb367d7f
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-11-07 16:59:15 +0800

    feat(agent): enhance Q-Learning and Dyna-Q agents with epsilon decay and learning updates

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import math
 import os
+import random
 import sys
 import time
 
@@ -80,49 +81,153 @@ class SarsaAgent(object):
 
 class QLearningAgent(object):
     ##### START CODING HERE #####
-    def __init__(self, all_actions):
+    def __init__(self, all_actions, learning_rate=0.1, gamma=0.9, epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.995):
         """initialize the agent. Maybe more function inputs are needed."""
         self.all_actions = all_actions
-        self.epsilon = 1.0
+        self.lr = learning_rate
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+
+        # Initialize Q-table as a dictionary
+        self.q_table = {}
 
     def choose_action(self, observation):
         """choose action with epsilon-greedy algorithm."""
-        action = np.random.choice(self.all_actions)
+        # Initialize Q-values for new state
+        if observation not in self.q_table:
+            self.q_table[observation] = np.zeros(len(self.all_actions))
+
+        # Epsilon-greedy action selection
+        if np.random.random() < self.epsilon:
+            # Exploration: choose random action
+            action = np.random.choice(self.all_actions)
+        else:
+            # Exploitation: choose best action
+            action = np.argmax(self.q_table[observation])
+
         return action
 
-    def learn(self):
-        """learn from experience"""
-        time.sleep(0.5)
-        print("(ﾉ｀⊿´)ﾉ")
-        return False
+    def learn(self, state, action, reward, next_state, done):
+        """learn from experience using Q-Learning algorithm (off-policy)"""
+        # Initialize Q-values for new states
+        if state not in self.q_table:
+            self.q_table[state] = np.zeros(len(self.all_actions))
+        if next_state not in self.q_table:
+            self.q_table[next_state] = np.zeros(len(self.all_actions))
 
-    def your_function(self, params):
-        """You can add other functions as you wish."""
-        return None
+        # Q-Learning update: Q(s,a) = Q(s,a) + lr * [r + gamma * max_a' Q(s',a') - Q(s,a)]
+        current_q = self.q_table[state][action]
+        if done:
+            # If terminal state, there is no next Q-value
+            target_q = reward
+        else:
+            # Use the maximum Q-value of next state (off-policy)
+            target_q = reward + self.gamma * np.max(self.q_table[next_state])
+
+        # Update Q-value
+        self.q_table[state][action] = current_q + self.lr * (target_q - current_q)
+
+        return True
+
+    def decay_epsilon(self):
+        """Decay epsilon value after each episode."""
+        self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
 
     ##### END CODING HERE #####
 
 
 class Dyna_QAgent(object):
     ##### START CODING HERE #####
-    def __init__(self, all_actions):
+    def __init__(self, all_actions, learning_rate=0.1, gamma=0.9, epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.995, planning_steps=5):
         """initialize the agent. Maybe more function inputs are needed."""
         self.all_actions = all_actions
-        self.epsilon = 1.0
+        self.lr = learning_rate
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.planning_steps = planning_steps  # Number of planning steps per real experience
+
+        # Initialize Q-table as a dictionary
+        self.q_table = {}
+
+        # Model: stores observed transitions (s, a) -> (r, s')
+        self.model = {}
 
     def choose_action(self, observation):
         """choose action with epsilon-greedy algorithm."""
-        action = np.random.choice(self.all_actions)
+        # Initialize Q-values for new state
+        if observation not in self.q_table:
+            self.q_table[observation] = np.zeros(len(self.all_actions))
+
+        # Epsilon-greedy action selection
+        if np.random.random() < self.epsilon:
+            # Exploration: choose random action
+            action = np.random.choice(self.all_actions)
+        else:
+            # Exploitation: choose best action
+            action = np.argmax(self.q_table[observation])
+
         return action
 
-    def learn(self):
-        """learn from experience"""
-        time.sleep(0.5)
-        print("(ﾉ｀⊿´)ﾉ")
-        return False
+    def learn(self, state, action, reward, next_state, done):
+        """learn from experience using Dyna-Q algorithm (Q-Learning + Planning)"""
+        # Initialize Q-values for new states
+        if state not in self.q_table:
+            self.q_table[state] = np.zeros(len(self.all_actions))
+        if next_state not in self.q_table:
+            self.q_table[next_state] = np.zeros(len(self.all_actions))
 
-    def your_function(self, params):
-        """You can add other functions as you wish."""
-        return None
+        # (a) Direct RL: Q-Learning update from real experience
+        current_q = self.q_table[state][action]
+        if done:
+            target_q = reward
+        else:
+            target_q = reward + self.gamma * np.max(self.q_table[next_state])
+
+        self.q_table[state][action] = current_q + self.lr * (target_q - current_q)
+
+        # (b) Model Learning: Store the transition in the model
+        if state not in self.model:
+            self.model[state] = {}
+        self.model[state][action] = (reward, next_state, done)
+
+        # (c) Planning: Learn from simulated experiences using the model
+        for _ in range(self.planning_steps):
+            # Randomly sample a previously observed state
+            if len(self.model) == 0:
+                break
+
+            # Sample a random state from the model
+            sampled_state = random.choice(list(self.model.keys()))
+
+            # Sample a random action that has been taken from this state
+            sampled_action = random.choice(list(self.model[sampled_state].keys()))
+
+            # Get the stored transition
+            sampled_reward, sampled_next_state, sampled_done = self.model[sampled_state][sampled_action]
+
+            # Initialize Q-values if needed
+            if sampled_state not in self.q_table:
+                self.q_table[sampled_state] = np.zeros(len(self.all_actions))
+            if sampled_next_state not in self.q_table:
+                self.q_table[sampled_next_state] = np.zeros(len(self.all_actions))
+
+            # Q-Learning update from simulated experience
+            current_q_sim = self.q_table[sampled_state][sampled_action]
+            if sampled_done:
+                target_q_sim = sampled_reward
+            else:
+                target_q_sim = sampled_reward + self.gamma * np.max(self.q_table[sampled_next_state])
+
+            self.q_table[sampled_state][sampled_action] = current_q_sim + self.lr * (target_q_sim - current_q_sim)
+
+        return True
+
+    def decay_epsilon(self):
+        """Decay epsilon value after each episode."""
+        self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
 
     ##### END CODING HERE #####
